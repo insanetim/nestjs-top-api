be849d7e9a10d2504ea78b6507bf38980e3d75b2
insanetim <47120724+insanetim@users.noreply.github.com>
2024-02-25 14:44:05 +0200

    update mongo.config

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -5,22 +5,10 @@ export const getMongoConfig = async (
   configService: ConfigService
 ): Promise<MongooseModuleFactoryOptions> => {
   return {
-    uri: getMongoString(configService),
+    uri: configService.get<string>('MONGO_URI'),
     ...getMongoOptions(),
   }
 }
-
-const getMongoString = (configService: ConfigService) =>
-  'mongodb://' +
-  configService.get<string>('MONGO_LOGIN') +
-  ':' +
-  configService.get<string>('MONGO_PASSWORD') +
-  '@' +
-  configService.get<string>('MONGO_HOST') +
-  ':' +
-  configService.get<string>('MONGO_PORT') +
-  '/' +
-  configService.get<string>('MONGO_AUTHDATABASE')
 
 const getMongoOptions = (): MongooseModuleFactoryOptions => ({
   retryWrites: true,
